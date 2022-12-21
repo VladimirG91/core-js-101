6359ceb6b6166f1f3313b9ecf3ceb751842af122
VladimirG91 <vladimir.glazkov91@gmail.com>
2022-12-21 11:25:36 +0600

    feat: add some solutions of array tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -525,8 +525,16 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const result = array.reduce((acc, el) => {
+    if (!(keySelector(el) in acc)) {
+      acc[keySelector(el)] = [valueSelector(el)];
+    } else {
+      acc[keySelector(el)].push(valueSelector(el));
+    }
+    return acc;
+  }, {});
+  return new Map(Object.entries(result));
 }
 
 
@@ -543,8 +551,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map(childrenSelector).flat();
 }
 
 
